6b7363c0b27bf9d369fcac708bd7e2221e074071
Ori <fogelori@gmail.com>
2024-09-22 10:35:19 +0300

    add third-party package (style-to-object) tests

---
+++ tests/js/style-to-object/style-to-object-esm/style-to-object-esm.test.js
@@ -0,0 +1,17 @@
+const pluginTransform = require("../../../pluginTransform");
+const ospath = require("path");
+
+describe("third-party package (style-to-object) transformation for ESM", () => {
+  test("transformation of style-to-object", () => {
+      const pluginOptions = { isCacheEnabled: true, logging: {type: "file"} };
+      expect(
+        pluginTransform(
+          'import style from "style-to-object";',
+          __filename,
+          pluginOptions
+        )
+      ).toBe([
+      `import style from \"style-to-object\\index.js";`,
+      ].join("\n").replaceAll("\\","\\\\"));
+  });
+});
