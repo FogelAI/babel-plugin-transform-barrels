1971a8f2058991939132bd7b6c4059b93122380a
Ori <fogelori@gmail.com>
2024-11-10 15:57:46 +0200

    add react-native support

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -1,9 +1,15 @@
 const babelJest = require("babel-jest").default;
 
+function normalizeExtensions(moduleFileExtensions, defaultPlatform) {
+  if (!defaultPlatform) return moduleFileExtensions;
+  const normalizedExtensions = moduleFileExtensions.map(ext => `${defaultPlatform}.${ext}`);
+  return [...moduleFileExtensions, ...normalizedExtensions];
+}
+
 module.exports = {
   process(sourceText, sourcePath, options) {
     const alias = options?.config?.moduleNameMapper;
-    const extensions = options.config.moduleFileExtensions;
+    const extensions = normalizeExtensions(options.config.moduleFileExtensions, options?.config?.haste?.defaultPlatform);
     const babelTransformer = babelJest.createTransformer({
       presets: [["@babel/preset-react"], ["@babel/preset-env"]],
       plugins: [["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions }]],
