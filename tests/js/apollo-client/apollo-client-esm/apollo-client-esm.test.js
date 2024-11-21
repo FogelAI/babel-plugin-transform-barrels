fbcfb01d7a8c9ea6daa248dfd85c1af63e981e32
Ori <fogelori@gmail.com>
2024-11-21 17:20:01 +0200

    add third-party package (@apollo/client) tests

---
+++ tests/js/apollo-client/apollo-client-esm/apollo-client-esm.test.js
@@ -0,0 +1,17 @@
+const pluginTransform = require("../../../pluginTransform");
+const ospath = require("path");
+
+describe("third-party package (@apollo/client/testing) transformation for ESM", () => {
+  test("transformation of @apollo/client/testing", () => {
+      const pluginOptions = { isCacheEnabled: true };
+      expect(
+        pluginTransform(
+          'import { MockedProvider } from "@apollo/client/testing";',
+          __filename,
+          pluginOptions
+        )
+      ).toBe([
+      `import { MockedProvider } from "@apollo/client/testing";`,
+      ].join("\n").replaceAll("\\","\\\\"));
+  });
+});
