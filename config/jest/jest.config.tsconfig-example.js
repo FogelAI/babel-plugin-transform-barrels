b647aae01a39d26c5c5cf9a22fbb902f7c87e517
Ori <fogelori@gmail.com>
2024-12-07 08:00:26 +0200

    add example jest.config.js file for typescript

---
+++ config/jest/jest.config.tsconfig-example.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'utf-8'));
+
+function convertPathsToModuleNameMapper(tsconfig) {
+ const { paths } = tsconfig.compilerOptions || {};
+
+ if (!paths) {
+   return {};
+ }
+
+ const moduleNameMapper = {};
+ for (const alias of Object.keys(paths)) {
+   const cleanPaths = paths[alias].map(path=> path.replace('/*', '/$1'));
+   const cleanAlias = alias.replace('/*', '/(.*)');
+   moduleNameMapper[`^${cleanAlias}$`] = cleanPaths;
+ }
+
+ return moduleNameMapper;
+}
+
+module.exports = {
+  verbose: true,
+  roots: ["<rootDir>"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    ...convertPathsToModuleNameMapper(tsconfig),
+  },
+  modulePaths: [tsconfig.compilerOptions.baseUrl],
+  testMatch: [
+    "<rootDir>/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/**/*.{spec,test}.{js,jsx,ts,tsx}",
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': require("path").resolve(__dirname, "./babelTransform.js"),
+  },
+  testPathIgnorePatterns: [
+    "/node_modules/",
+  ],
+};
